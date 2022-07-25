51fc61cb932b788f7d171357a693a2e0301d3e5c
Riccardo Biffi <biffi.riccardo91@gmail.com>
2022-07-25 20:39:34 +0200

    Add token mint

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract RewardToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Reward", "RWD") {}
+    constructor(uint256 initialSupply) ERC20("Reward", "RWD") {
+        _mint(msg.sender, initialSupply * 10**18);
+    }
 }
