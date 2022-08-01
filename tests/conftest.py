e021ae3c7309275ac72b193c05a6915852df0315
Riccardo Biffi <biffi.riccardo91@gmail.com>
2022-08-01 21:28:33 +0200

    Refact test, add issueRewardeTokens tests

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+from web3 import Web3
+import pytest
+
+
+@pytest.fixture
+def amount_staked():
+    return Web3.toWei(1, "ether")
--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -1,7 +1,13 @@
-from brownie import TokenFarm, RewardToken
 from brownie import network, exceptions
 import pytest
-from scripts.utilities import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from scripts.utilities import (
+    MockContract,
+    get_account,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_contract,
+    DECIMALS,
+)
+from scripts.deploy import deploy_token_farm_and_dapp_token
 
 
 def test_can_deploy_token_farm():
@@ -9,47 +15,192 @@ def test_can_deploy_token_farm():
         pytest.skip("Only for local testing")
 
     # Arrange
-    account = get_account()
-    rewardToken = "0x7777cBDC3dbcd3E11a07892e7bA5c33940487777"
 
     # Act
-    tf = TokenFarm.deploy(rewardToken, {"from": account})
+    token_farm, _ = deploy_token_farm_and_dapp_token()
 
     # Assert
-    assert tf.address != None
+    assert token_farm.address != None
 
 
-def test_can_allow_tokens():
+# region stakeTokens
+def test_stake_token_success(amount_staked):
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip("Only for local testing")
 
     # Arrange
     account = get_account()
-    token_addr = "0x4194cBDC3dbcd3E11a07892e7bA5c3394048Cc87"
-    rewardToken = "0x7777cBDC3dbcd3E11a07892e7bA5c33940487777"
+    token_farm, reward_token = deploy_token_farm_and_dapp_token()
+    previous_balance = reward_token.balanceOf(token_farm.address)
 
     # Act
-    tf = TokenFarm.deploy(rewardToken, {"from": account})
-    tf.addAllowedToken(token_addr, {"from": account})
+    reward_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeTokens(amount_staked, reward_token.address, {"from": account})
 
     # Assert
-    assert tf.allowedTokens(0) == "0x4194cBDC3dbcd3E11a07892e7bA5c3394048Cc87"
+    assert (
+        token_farm.token_staker_amount(reward_token.address, account) == amount_staked
+    )
+    assert token_farm.stakers(0) == account
+    assert token_farm.staker_uniqueTokenNumber(account) == 1
+    assert (
+        reward_token.balanceOf(token_farm.address) - previous_balance == amount_staked
+    )
+    return token_farm, reward_token
 
 
+def test_stake_token_fail_low_amount(amount_staked):
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+
+    # Arrange
+    account = get_account()
+    token_farm, reward_token = deploy_token_farm_and_dapp_token()
+
+    # Act
+    reward_token.approve(token_farm.address, amount_staked, {"from": account})
+
+    # Assert
+    with pytest.raises(exceptions.VirtualMachineError):
+        token_farm.stakeTokens(0, reward_token.address, {"from": account})
+
+
+def test_stake_token_fail_not_allowed(amount_staked):
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+
+    # Arrange
+    account = get_account()
+    token_farm, reward_token = deploy_token_farm_and_dapp_token()
+    not_allowed_token = "0x4194cBDC3dbcd3E11a07892e7bA5c3394048Cc87"
+
+    # Act
+    reward_token.approve(token_farm.address, amount_staked, {"from": account})
+
+    # Assert
+    with pytest.raises(exceptions.VirtualMachineError):
+        token_farm.stakeTokens(10**18, not_allowed_token, {"from": account})
+
+
+# endregion
+
+# region unstakeTokens
+def test_unstake_token_success(amount_staked):
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+
+    # Arrange
+    account = get_account()
+    token_farm, reward_token = test_stake_token_success(amount_staked)
+    previous_balance = reward_token.balanceOf(account)
+
+    # Act
+    token_farm.unstakeTokens(reward_token.address, {"from": account})
+
+    # Assert
+    assert reward_token.balanceOf(account) == previous_balance + amount_staked
+    assert token_farm.token_staker_amount(reward_token.address, account) == 0
+    assert token_farm.staker_uniqueTokenNumber(account) == 0
+    # hack check for empty stakers array
+    with pytest.raises(exceptions.VirtualMachineError):
+        assert token_farm.stakers(0)
+
+
+def test_unstake_token_fail_not_allowed():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+
+    # Arrange
+    account = get_account()
+    not_allowed_token = "0x4194cBDC3dbcd3E11a07892e7bA5c3394048Cc87"
+    token_farm, _ = deploy_token_farm_and_dapp_token()
+
+    # Act
+
+    # Assert
+    with pytest.raises(exceptions.VirtualMachineError):
+        token_farm.unstakeTokens(not_allowed_token, {"from": account})
+
+
+def test_unstake_token_fail_zero_balance():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+
+    # Arrange
+    account = get_account()
+    token_farm, reward_token = deploy_token_farm_and_dapp_token()
+
+    # Act
+
+    # Assert
+    with pytest.raises(exceptions.VirtualMachineError):
+        token_farm.unstakeTokens(reward_token, {"from": account})
+
+
+# endregion
+
+# region issueRerwardTokens
+def test_issue_reward_tokens_success(amount_staked):
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+
+    # Arrange
+    account = get_account()
+    token_farm, reward_token = test_stake_token_success(amount_staked)
+    starting_balance = reward_token.balanceOf(account)
+
+    # Act
+    token_farm.issueRewardTokens({"from": account})
+
+    # Assert
+    assert reward_token.balanceOf(account) == starting_balance + 10**DECIMALS
+
+
+def test_issue_reward_tokens_fail_no_owner():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+
+    # Arrange
+    not_owner = get_account(1)
+    token_farm, _ = deploy_token_farm_and_dapp_token()
+
+    # Act
+
+    # Assert
+    with pytest.raises(exceptions.VirtualMachineError):
+        token_farm.issueRewardTokens({"from": not_owner})
+
+
+# endregion
+
+
+def test_add_allowed_tokens():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+
+    # Arrange
+    account = get_account()
+    token_farm, reward_token = deploy_token_farm_and_dapp_token()
+    token_farm.addAllowedToken(reward_token.address, {"from": account})
+
+    # Act
+
+    # Assert
+    assert token_farm.allowedTokens(0) == reward_token.address
+
+
+# region isTokenAllowed
 def test_check_token_allowed_true():
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip("Only for local testing")
 
     # Arrange
     account = get_account()
-    token_addr = "0x4194cBDC3dbcd3E11a07892e7bA5c3394048Cc87"
-    rewardToken = "0x7777cBDC3dbcd3E11a07892e7bA5c33940487777"
-
-    tf = TokenFarm.deploy(rewardToken, {"from": account})
-    tf.addAllowedToken(token_addr, {"from": account})
+    token_farm, reward_token = deploy_token_farm_and_dapp_token()
+    token_farm.addAllowedToken(reward_token.address, {"from": account})
 
     # Act
-    answer = tf.isTokenAllowed(token_addr, {"from": account})
+    answer = token_farm.isTokenAllowed(reward_token, {"from": account})
 
     # Assert
     assert answer == True
@@ -61,75 +212,18 @@ def test_check_token_allowed_false():
 
     # Arrange
     account = get_account()
-    token_addr = "0x4194cBDC3dbcd3E11a07892e7bA5c3394048Cc87"
-    rewardToken = "0x7777cBDC3dbcd3E11a07892e7bA5c33940487777"
-    other_token_addr = "0x9294cBDC3dbcd3E11a07892e7bA5c3394048Cc51"
-    tf = TokenFarm.deploy(rewardToken, {"from": account})
-    tf.addAllowedToken(token_addr, {"from": account})
+    token_farm, reward_token = deploy_token_farm_and_dapp_token()
+    not_allowed_token = "0x4194cBDC3dbcd3E11a07892e7bA5c3394048Cc87"
+    token_farm.addAllowedToken(reward_token.address, {"from": account})
 
     # Act
-    answer = tf.isTokenAllowed(other_token_addr, {"from": account})
+    answer = token_farm.isTokenAllowed(not_allowed_token, {"from": account})
 
     # Assert
     assert answer == False
 
 
-def test_stake_token_success():
-    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-        pytest.skip("Only for local testing")
-
-    # Arrange
-    account = get_account()
-    amount = 10**18
-    rewardToken = "0x4194CBDC3DBCD3E11A07892E7BA5C3394048CC87"
-    token = RewardToken.deploy(10, {"from": account})
-    tf = TokenFarm.deploy(rewardToken, {"from": account})
-
-    token.approve(tf.address, amount, {"from": account})
-    tf.addAllowedToken(token.address, {"from": account})
-
-    # Act
-    tf.stakeTokens(amount, token.address, {"from": account})
-
-    # Assert
-    assert tf.token_staker_amount(token.address, account) == amount
-    assert tf.stakers(0) == account
-    assert tf.staker_uniqueTokenNumber(account) == 1
-    assert token.balanceOf(tf.address) == amount
-
-
-def test_stake_token_fail_low_amount():
-    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-        pytest.skip("Only for local testing")
-
-    # Arrange
-    account = get_account()
-    token_addr = "0x4194cBDC3dbcd3E11a07892e7bA5c3394048Cc87"
-    rewardToken = "0x7777cBDC3dbcd3E11a07892e7bA5c33940487777"
-    tf = TokenFarm.deploy(rewardToken, {"from": account})
-
-    # Act
-
-    # Assert
-    with pytest.raises(exceptions.VirtualMachineError):
-        tf.stakeTokens(0, token_addr, {"from": account})
-
-
-def test_stake_token_fail_not_allowed():
-    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-        pytest.skip("Only for local testing")
-
-    # Arrange
-    account = get_account()
-    token_addr = "0x4194cBDC3dbcd3E11a07892e7bA5c3394048Cc87"
-    rewardToken = "0x7777cBDC3dbcd3E11a07892e7bA5c33940487777"
-    tf = TokenFarm.deploy(rewardToken, {"from": account})
-
-    # Act
-
-    # Assert
-    with pytest.raises(exceptions.VirtualMachineError):
-        tf.stakeTokens(10**18, token_addr, {"from": account})
+# endregion
 
 
 def test_set_token_price_feed_success():
@@ -138,16 +232,16 @@ def test_set_token_price_feed_success():
 
     # Arrange
     account = get_account()
-    token_addr = "0x4194cBDC3dbcd3E11a07892e7bA5c3394048Cc87"
-    rewardToken = "0x7777cBDC3dbcd3E11a07892e7bA5c33940487777"
-    price_feed_addr = "0x9999cBDC3dbcd3E11a07892e7bA5c33940489999"
-    tf = TokenFarm.deploy(rewardToken, {"from": account})
+    token_farm, reward_token = deploy_token_farm_and_dapp_token()
+    price_feed_addr = get_contract(MockContract.ETH_USD_FEED)
 
     # Act
-    tf.setTokenPriceFeed(token_addr, price_feed_addr, {"from": account})
+    token_farm.setTokenPriceFeed(
+        reward_token.address, price_feed_addr, {"from": account}
+    )
 
     # Assert
-    assert tf.token_priceFeed(token_addr) == price_feed_addr
+    assert token_farm.token_priceFeed(reward_token.address) == price_feed_addr
 
 
 def test_set_token_price_feed_fail_no_owner():
@@ -155,15 +249,14 @@ def test_set_token_price_feed_fail_no_owner():
         pytest.skip("Only for local testing")
 
     # Arrange
-    admin = get_account()
-    user = get_account(1)
-    token_addr = "0x4194cBDC3dbcd3E11a07892e7bA5c3394048Cc87"
-    rewardToken = "0x7777cBDC3dbcd3E11a07892e7bA5c33940487777"
-    price_feed_addr = "0x9999cBDC3dbcd3E11a07892e7bA5c33940489999"
-    tf = TokenFarm.deploy(rewardToken, {"from": admin})
+    not_owner = get_account(1)
+    token_farm, reward_token = deploy_token_farm_and_dapp_token()
+    price_feed_addr = get_contract(MockContract.ETH_USD_FEED)
 
     # Act
 
     # Assert
     with pytest.raises(exceptions.VirtualMachineError):
-        tf.setTokenPriceFeed(token_addr, price_feed_addr, {"from": user})
+        token_farm.setTokenPriceFeed(
+            reward_token.address, price_feed_addr, {"from": not_owner}
+        )
