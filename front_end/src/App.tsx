e20ef2babe33e0f32a1eaeec4dc8fa93a5d5255e
Riccardo Biffi <biffi.riccardo91@gmail.com>
2022-08-06 21:38:22 +0200

    Update App with metamask connection

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom'
+
+import { Rinkeby, DAppProvider, Config } from '@usedapp/core'
+import { formatEther } from '@ethersproject/units'
+import { getDefaultProvider } from 'ethers'
+import { Header } from './components/Header'
+
+const config: Config = {
+  readOnlyChainId: Rinkeby.chainId,
+  readOnlyUrls: {
+    [Rinkeby.chainId]: getDefaultProvider('rinkeby'),
+  },
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DAppProvider config={config}>
+      <Header />
+      <div>Hi!</div>
+    </DAppProvider>
   );
 }
 
