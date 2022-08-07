556bcd6c59192493131f98124c46a9c901a75123
Riccardo Biffi <biffi.riccardo91@gmail.com>
2022-08-07 21:35:17 +0200

    Update App and Header components

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-
-import { Rinkeby, DAppProvider, Config } from '@usedapp/core'
-import { formatEther } from '@ethersproject/units'
+import { Rinkeby, Kovan, DAppProvider, Config } from '@usedapp/core'
 import { getDefaultProvider } from 'ethers'
 import { Header } from './components/Header'
+import { Main } from './components/Main'
+import { Container } from '@mui/material'
 
+// todo refactor to include ganache (chain id 1337)
 const config: Config = {
-  readOnlyChainId: Rinkeby.chainId,
+  readOnlyChainId: Kovan.chainId,
   readOnlyUrls: {
-    [Rinkeby.chainId]: getDefaultProvider('rinkeby'),
+    [Kovan.chainId]: getDefaultProvider('kovan'),
   },
 }
 
 function App() {
   return (
     <DAppProvider config={config}>
-      <Header />
-      <div>Hi!</div>
+      <Container maxWidth="lg">
+        <Header />
+        <Main />
+      </Container>
     </DAppProvider>
   );
 }
--- front_end/src/components/Header.tsx
+++ front_end/src/components/Header.tsx
@@ -1,21 +1,32 @@
 import { useEthers } from "@usedapp/core";
+import { Button } from "@mui/material";
+import styled from "@emotion/styled"
 
+
+const Container = styled.div`
+    padding: 8px;
+    display: flex;
+    justify-content: flex-end;
+`
 export const Header = () => {
     const { account, activateBrowserWallet, deactivate } = useEthers();
     const isConnected = account !== undefined;
 
     return (
-        <div>
-            {isConnected ? (
-                <button color="primary" onClick={deactivate}>Disconnect</button>
-            )
-                : (
-                    <button color="primary"
-                        onClick={() => activateBrowserWallet()}>
-                        Connect
-                    </button>
+        <Container >
+            <div>
+                {isConnected ? (
+                    <Button color="primary" variant="contained"
+                        onClick={() => deactivate()}>Disconnect</Button>
                 )
-            }
-        </div>
+                    : (
+                        <Button color="primary" variant="contained"
+                            onClick={() => activateBrowserWallet()}>
+                            Connect
+                        </Button>
+                    )
+                }
+            </div>
+        </Container >
     )
 }
