5078e81c5d6aa8c86d34293e1472416043c91f4e
Riccardo Biffi <biffi.riccardo91@gmail.com>
2022-08-09 17:34:19 +0200

    Update Reward Token supply in wei.

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract RewardToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Reward", "RWD") {
-        _mint(msg.sender, initialSupply * 10**18);
+        _mint(msg.sender, initialSupply);
     }
 }
