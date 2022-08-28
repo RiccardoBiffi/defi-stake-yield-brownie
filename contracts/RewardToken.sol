844d4f950194746042c0b3478b9185f33eddd9b5
Riccardo Biffi <biffi.riccardo91@gmail.com>
2022-08-28 22:49:27 +0200

    WIP update reward token to buy with other tokens

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -1,10 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract RewardToken is ERC20 {
+contract RewardToken is ERC20, Ownable {
+    address[] public allowedTokens;
+    mapping(address => address) public token_priceFeed;
+
     constructor(uint256 initialSupply) ERC20("Reward", "RWD") {
         _mint(msg.sender, initialSupply);
+    }
+
+    function buy(uint256 amount, address exchangeToken) public {
+        IERC20(exchangeToken).transferFrom(msg.sender, address(this), amount);
+        // compute RWD value
+        uint256 buyedTokens = 10**18;
+        transfer(msg.sender, buyedTokens);
+    }
+
+    function addAllowedToken(address _token) public onlyOwner {
+        allowedTokens.push(_token);
+    }
+
+    function isTokenAllowed(address _token) public view returns (bool) {
+        for (uint256 i = 0; i < allowedTokens.length; i++) {
+            if (allowedTokens[i] == _token) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function setTokenPriceFeed(address token, address priceFeed)
+        public
+        onlyOwner
+    {
+        token_priceFeed[token] = priceFeed;
     }
 }
