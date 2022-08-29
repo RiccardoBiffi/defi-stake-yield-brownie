4c1d61b1055ec5f524c9c30274c29e8533927477
Riccardo Biffi <biffi.riccardo91@gmail.com>
2022-08-29 23:11:57 +0200

    Update reward token to buy tokens with others

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -1,13 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./AllowTokens.sol";
+import "./TokenValue.sol";
 
-contract RewardToken is ERC20, Ownable {
-    address[] public allowedTokens;
-    mapping(address => address) public token_priceFeed;
+contract RewardToken is ERC20, AllowTokens, TokenValue {
+    address[] public tokenWithDeposits;
+    mapping(address => uint256) public token_deposit;
+
+    event Exchange(
+        address from,
+        address exchangeToken,
+        uint256 amountIn,
+        uint256 amountRwdOut
+    );
+    event CashOut(address admin);
 
     constructor(uint256 initialSupply) ERC20("Reward", "RWD") {
         _mint(msg.sender, initialSupply);
@@ -15,26 +24,32 @@ contract RewardToken is ERC20, Ownable {
 
     function buy(uint256 amount, address exchangeToken) public {
         IERC20(exchangeToken).transferFrom(msg.sender, address(this), amount);
-        // compute RWD value
-        uint256 buyedTokens = 10**18;
-        transfer(msg.sender, buyedTokens);
+        tokenWithDeposits.push(exchangeToken);
+        token_deposit[exchangeToken] = amount;
+
+        uint256 valueSent = getValueFromToken(amount, exchangeToken);
+        uint256 buyedRwdTokens = getTokenFromValue(valueSent, address(this));
+        transfer(msg.sender, buyedRwdTokens);
+
+        emit Exchange(msg.sender, exchangeToken, amount, buyedRwdTokens);
     }
 
-    function addAllowedToken(address _token) public onlyOwner {
-        allowedTokens.push(_token);
-    }
-
-    function isTokenAllowed(address _token) public view returns (bool) {
-        for (uint256 i = 0; i < allowedTokens.length; i++) {
-            if (allowedTokens[i] == _token) {
-                return true;
-            }
+    function cashOut() public onlyOwner {
+        for (uint256 i = 0; i < tokenWithDeposits.length; i++) {
+            IERC20(tokenWithDeposits[i]).transfer(
+                owner(),
+                token_deposit[tokenWithDeposits[i]]
+            );
+            delete token_deposit[tokenWithDeposits[i]];
         }
-        return false;
+
+        delete tokenWithDeposits;
+        emit CashOut(msg.sender);
     }
 
     function setTokenPriceFeed(address token, address priceFeed)
         public
+        override
         onlyOwner
     {
         token_priceFeed[token] = priceFeed;
