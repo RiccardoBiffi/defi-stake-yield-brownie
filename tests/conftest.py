1f99d671cef2300377a82d544f528ddd9a232cd9
Riccardo Biffi <biffi.riccardo91@gmail.com>
2022-08-31 23:54:24 +0200

    Add tokenValue test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,17 @@
 from web3 import Web3
 import pytest
-from brownie import AllowTokens
-
-
+from brownie import AllowTokens, TokenValue, MockV3Aggregator
 from scripts.utilities import get_account
 
 
 @pytest.fixture
 def amount_staked():
     return Web3.toWei(1, "ether")
+
+
+@pytest.fixture
+def decimals():
+    return 18
 
 
 @pytest.fixture
@@ -19,6 +22,21 @@ def account():
 @pytest.fixture
 def allow_tokens(account):
     return AllowTokens.deploy({"from": account})
+
+
+@pytest.fixture
+def token_value(account):
+    return TokenValue.deploy({"from": account})
+
+
+@pytest.fixture
+def token_value_DAI(decimals, account):
+    return MockV3Aggregator.deploy(decimals, 1 * 10**decimals, {"from": account})
+
+
+@pytest.fixture
+def token_value_ETH(decimals, account):
+    return MockV3Aggregator.deploy(decimals, 1560 * 10**decimals, {"from": account})
 
 
 @pytest.fixture
