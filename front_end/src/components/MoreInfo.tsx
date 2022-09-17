df38cedb55c5bad56b3ded88b0418fe603c8cc04
Riccardo Biffi <biffi.riccardo91@gmail.com>
2022-09-18 00:03:13 +0200

    Add MoreInfo component

--- front_end/src/components/MoreInfo.tsx
+++ front_end/src/components/MoreInfo.tsx
@@ -1,11 +1,4 @@
-import { Token } from "../Main";
 import { useEthers } from "@usedapp/core";
-import { formatUnits } from "ethers/lib/utils";
-import { BalanceMsg } from "../BalanceMsg"
-import { useTokenStakerAmount } from "../../hooks/useTokenStakerAmount"
-import { formatBigNumber } from "../../utils";
-
-
 
 export const MoreInfo = () => {
     const { account } = useEthers();
